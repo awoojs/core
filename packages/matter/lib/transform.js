979d228e6b1cb0a7b4957f197c163f36ca2f4fe1
Frederic Marx <marx.frederic@gmail.com>
2017-09-20 20:26:11 +0200

    Matter: Preserve existing metadata

--- packages/matter/lib/transform.js
+++ packages/matter/lib/transform.js
@@ -3,12 +3,13 @@ const isEmptyObject = require('is-empty-object')
 
 function matter (file, options, files) {
   const grayMatterOutput = grayMatter(file.contents, options)
+  const metadata = Object.assign({}, file.metadata, grayMatterOutput.data)
 
   const newFile = isEmptyObject(grayMatterOutput.data)
     ? file
     : Object.assign(file, {
       contents: grayMatterOutput.content,
-      metadata: grayMatterOutput.data
+      metadata
     })
 
   return newFile
